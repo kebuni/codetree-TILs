1d4d53db50a86ecb08dd270ddef6e7cc214b9a61
kebuni <121942412+kebuni@users.noreply.github.com>
2024-01-23 23:09:11 +0900

    Title: 거울에 레이저 쏘기 2, Time: 116ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240123/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -0,0 +1,54 @@
+import sys
+
+N = int(input())
+
+dxs = [-1,1,0,0]
+dys = [0,0,1,-1]
+direction = -1
+x, y = -1, -1
+ans = 0
+
+grid = [[1]*N for _ in range(N)]
+
+def in_range(x,y):
+    return 0<=x<N and 0<=y<N
+
+def reflect(x,y,direction):
+    if grid[x][y] == 0:
+        if direction >1:
+            return 5 - direction
+        else:
+            return 1 - direction
+    else:
+        return 3-direction
+
+for i in range(N):
+    line = sys.stdin.readline()
+    for j in range(N):
+        if line[j] == '/':
+            grid[i][j] = 0
+
+light = int(input())
+
+### 초기 위치와 방향 잡기 ###
+
+if 1<=light<=N:
+    direction = 1
+    x = 0; y = light - 1
+elif N+1<=light<=N*2:
+    direction = 3
+    y = N-1; x = (light -1 ) % N
+elif N*2+1<=light<=N*3:
+    x = N-1; y = (4*N - light) % N
+else:
+    y = 0; x = 4*N - light
+
+while in_range(x,y):
+    #print(x,y,direction)
+    direction = reflect(x,y,direction)
+    #print("new_dir: ",direction)
+    x = x + dxs[direction]
+    y = y + dys[direction]
+    ans = ans + 1
+
+print(ans)
