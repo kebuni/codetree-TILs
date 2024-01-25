ec272328567fdf6d92b5138f97eeaacd812693b4
kebuni <121942412+kebuni@users.noreply.github.com>
2024-01-25 21:33:56 +0900

    Title: 줄 세우기, Time: 155ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240125/줄 세우기/line-up-students.py
@@ -0,0 +1,11 @@
+N = int(input())
+
+arr = []
+for i in range(N):
+    h, w = map(int,input().split())
+    arr.append((h,w,i+1))
+
+arr.sort(key=lambda x:(-x[0],-x[1],x[2]))
+
+for a,b,c in arr:
+    print(a,b,c)
