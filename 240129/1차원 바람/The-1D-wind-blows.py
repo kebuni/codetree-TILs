aa5d21bd58a833994ad71678c2791aff04f2ee75
kebuni <121942412+kebuni@users.noreply.github.com>
2024-01-29 20:51:44 +0900

    Title: 1차원 바람, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240129/1차원 바람/The-1D-wind-blows.py
@@ -0,0 +1,65 @@
+N, M, Q = map(int, input().split())
+
+grid = []
+
+for i in range(N):
+    grid.append(list(map(int, input().split())))
+
+start_row, start_direct = input().split()
+start_row = int(start_row) - 1
+
+###########################
+
+directs = {'R': 0, 'L': 1}
+visited = [False]*N
+
+def clear_visited():
+    for i in range(N):
+        visited[i] = False
+def wind(row, direct):
+    visited[row] = True
+    if direct == 1:
+        temp = grid[row][M - 1]
+        for i in range(M - 1, 0, -1):
+            grid[row][i] = grid[row][i - 1]
+        grid[row][0] = temp
+    else:
+        temp = grid[row][0]
+        for i in range(M - 1):
+            grid[row][i] = grid[row][i + 1]
+        grid[row][M - 1] = temp
+
+    if propa(row, row - 1):
+        wind(row - 1, (direct + 1) % 2)
+
+    if propa(row, row + 1):
+        wind(row + 1, (direct + 1) % 2)
+
+    return
+
+
+def propa(cur, new):
+
+    if new < 0 or new >= N:
+        return False
+
+    if visited[new] == True:
+        return False
+
+    for i in range(M):
+        if grid[cur][i] == grid[new][i]:
+            return True
+
+    return False
+
+
+###########################
+
+for i in range(Q):
+    wind(start_row, directs[start_direct])
+    clear_visited()
+
+for i in range(N):
+    for elem in grid[i]:
+        print(elem, end=' ')
+    print()
