c9abffe91fea8a8bd2d9f825ef7bbfde267950ff
kebuni <121942412+kebuni@users.noreply.github.com>
2024-02-07 16:10:45 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 160ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240207/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
@@ -0,0 +1,38 @@
+from collections import deque
+q = deque()
+####################
+def bfs():
+    while q:
+        x= q.popleft()
+
+        if x>1 and not visited[x-1]:
+            push(x-1,step[x]+1)
+        
+        if x<N and not visited[x+1]:
+            push(x+1,step[x]+1)
+
+        if x%2==0 and not visited[x//2]:
+            push(x//2,step[x]+1)
+
+        if x%3==0 and not visited[x//3]:
+            push(x//3,step[x]+1)
+
+    return
+
+def push(x,s):
+    visited[x] = 1
+    step[x] = s
+    #print(step)
+    q.append(x)
+    return
+
+####################
+N = int(input())
+
+step = [-1 for i in range(N+1)]
+visited = [0 for i in range(N+1)]
+
+push(N,0)
+bfs()
+
+print(step[1])
