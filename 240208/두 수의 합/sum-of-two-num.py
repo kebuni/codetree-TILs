e5e81ae50e4667fc8a36bb55056a6970e7285879
kebuni <121942412+kebuni@users.noreply.github.com>
2024-02-08 09:27:00 +0900

    Title: 두 수의 합, Time: 126ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240208/두 수의 합/sum-of-two-num.py
@@ -0,0 +1,24 @@
+import sys
+
+dic = {}
+
+N, K = map(int,input().split())
+line = list(map(int,sys.stdin.readline().split()))
+deleted_dic = {}
+
+for elem in line:
+    if elem in dic:
+        dic[elem] += 1
+    else:
+        dic[elem] = 1
+
+ans = 0
+for key in dic:
+    #print("####",key,"####")
+    if K-key in dic and K-key not in deleted_dic and key not in deleted_dic:
+        #print(K-key,' : ',dic[K-key])
+        ans += dic[key]*dic[K-key]
+        deleted_dic[key] = 0
+        deleted_dic[K-key] = 0
+
+print(ans)
