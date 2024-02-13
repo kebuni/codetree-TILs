86d981b401eaf214b6222bfa8ed6d0a99c1e7066
kebuni <121942412+kebuni@users.noreply.github.com>
2024-02-13 17:24:20 +0900

    Title: 초대장과 번호표, Time: 303ms, Memory: 45MB, Status: Passed - CodeTree

--- 240213/초대장과 번호표/invitation-and-number-tag.py
+++ 240213/초대장과 번호표/invitation-and-number-tag.py
@@ -1,41 +1,38 @@
 from collections import deque
-
-# 변수 선언 및 입력:
-n, g = tuple(map(int, input().split()))
-
-invited = [False] * n
-# 각 그룹마다 초대장을 받지 못한 사람들을 관리해줍니다.
-groups = [set() for _ in range(g)]
-# 각 사람이 어떤 그룹에 속하는지를 관리해줍니다.
-people_groups = [[] for _ in range(n)]
-
-q = deque()
 ans = 0
+#####################
 
-for i in range(g):
-    nums = list(map(int, input().split()))[1:]
-    for num in nums:
-        num -= 1
-        groups[i].add(num)
-        people_groups[num].append(i)
+#####################
 
-q.append(0)
-invited[0] = True
-while q:
-    x = q.popleft()
+N, K = map(int,input().split())
+group_list = [set() for i in range(N+1)]
+group_invitation_list = [set() for i in range(K)]
+invited = [False for i in range(N+1)]
+check_list = deque()
+
+for g in range(K):
+    line = list(map(int,input().split()))
+    group_num = line[0]
+    for i in range(1,group_num+1):
+        group_list[line[i]].add(g)
+        group_invitation_list[g].add(line[i])
+
+#print(group_list)
+check_list.append(1)
+invited[1] = True
+        
+while check_list:
+
+    x = check_list.popleft()
     ans += 1
 
-    # x가 들어있는 그룹에서 x를 지웁니다.
-    # hashset에는 그룹에서 초대받지 않은 인원만을 남깁니다.
-    for g_num in people_groups[x]:
-        # 해당 그룹에서 x를 지웁니다.
-        groups[g_num].remove(x)
-        # 초대받지 않은 인원이 한명밖에 없다면 초대합니다.
-        if len(groups[g_num]) == 1:
-            p_num = list(groups[g_num])[0]
+    for group in group_list[x]:
+        
+        group_invitation_list[group].remove(x)
+        if len(group_invitation_list[group])==1:
+            p_num = list(group_invitation_list[group])[0]
             if not invited[p_num]:
                 invited[p_num] = True
-                q.append(p_num)
+                check_list.append(p_num)
 
-# 초대장을 받는 인원을 출력합니다.
 print(ans)
