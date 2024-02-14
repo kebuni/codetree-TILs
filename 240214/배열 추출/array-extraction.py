4172a74ae0912e64902f90e48d6cc8d783b8bdac
kebuni <121942412+kebuni@users.noreply.github.com>
2024-02-14 21:03:49 +0900

    Title: 배열 추출, Time: 372ms, Memory: 34MB, Status: Passed - CodeTree

--- 240214/배열 추출/array-extraction.py
+++ 240214/배열 추출/array-extraction.py
@@ -1,14 +1,25 @@
 import heapq
 
-N = int(input())
-q = []
+# 입력:
+n = int(input())
+arr = [
+    int(input())
+    for _ in range(n)
+]
 
-for i in range(N):
-    x = int(input())
-    if x == 0:
-        if len(q):
-            print(-heapq.heappop(q))
-        else:
-            print(0)
+# 변수 선언
+pq = []
+
+for x in arr:
+    if x != 0:
+        # x가 자연수라면
+        # priority queue에 x를 넣어줍니다.
+        heapq.heappush(pq, -x)
+    
     else:
-        heapq.heappush(q,-x)
+        # x가 0이라면
+        # 최댓값을 찾아 출력한 뒤 제거합니다.
+        if not pq:
+            print(0)
+        else:
+            print(-heapq.heappop(pq))
