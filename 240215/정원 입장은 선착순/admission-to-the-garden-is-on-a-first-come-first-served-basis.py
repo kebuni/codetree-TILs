0f8669e0c526c8928c4059d76246392767c0a3a2
kebuni <121942412+kebuni@users.noreply.github.com>
2024-02-15 12:06:16 +0900

    Title: 정원 입장은 선착순, Time: 260ms, Memory: 27MB, Status: Wrong Answer - CodeTree

--- 240215/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.py
+++ 240215/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.py
@@ -13,34 +13,27 @@ for i in range(N):
     customers.append((start,i,duration))
 
 customers.sort()
+cus_idx = 0
 
-# print(customers)
-# print(waiting_list)
-# print(ans)
-# print('----------------')
-
-cus_num = 0
-while cus_num < N or waiting_list:
+for i in range(N):
     
-    if not waiting_list: # 웨이팅 리스트에 누가 없으면
-        start, idx, duration = customers[cus_num]
-        cus_num += 1
+    if not waiting_list:
+        start, idx, duration = customers[cus_idx]
+        cus_idx += 1
+        ans = max(ans, end_time - start)
         end_time = start + duration
+
         while True:
-            if cus_num < N and customers[cus_num][0] < end_time:
-                start, idx, duration = customers[cus_num]
-                cus_num += 1
-                heapq.heappush(waiting_list,(idx,start,duration))
+            if cus_idx<N and customers[cus_idx][0] < end_time + duration:
+                start, idx, duration = customers[cus_idx]
+                cus_idx += 1
+                heapq.heappush(waiting_list,(idx, start, duration))
             else:
                 break
-    else: # 웨이팅 리스트에 누가 있다면
+
+    else:
         idx, start, duration = heapq.heappop(waiting_list)
-        ans = max(ans,end_time-start)
-        end_time = end_time + duration
-
-    # print(customers)
-    # print(waiting_list)
-    # print(ans)
-    # print('----------------')
-
+        ans = max(ans, end_time - start)
+        end_time += duration
+        
 print(ans)
