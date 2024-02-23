029443c2ac9f483090eeb3a87b5fe9b5441d80a5
kebuni <121942412+kebuni@users.noreply.github.com>
2024-02-23 14:10:56 +0900

    Title: 바이러스 검사, Time: 409ms, Memory: 123MB, Status: Passed - CodeTree

--- 240223/바이러스 검사/virus-detector.py
+++ 240223/바이러스 검사/virus-detector.py
@@ -7,6 +7,8 @@ rests = list(map(int,input().split()))
 manager, employee = map(int,input().split())
 
 for rest in rests:
+    if rest - manager <= 0:
+        continue
     ans += (rest - manager) // employee
     if (rest - manager) % employee:
         ans += 1
