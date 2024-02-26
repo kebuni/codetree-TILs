4229e31040612dca08497aaadc806a150a39a4e8
kebuni <121942412+kebuni@users.noreply.github.com>
2024-02-27 02:19:57 +0900

    Title: Carry 피하기 2, Time: 181ms, Memory: 24MB, Status: Passed - CodeTree

--- 240227/Carry 피하기 2/escaping-carry-2.py
+++ 240227/Carry 피하기 2/escaping-carry-2.py
@@ -18,7 +18,7 @@ def check_carry(a,b,c):
 
 for i in range(N):
     for j in range(i+1,N):
-        for k in range(i+1,N):
+        for k in range(j+1,N):
             if check_carry(arr[i],arr[j],arr[k]):
                 ans = max(ans,arr[i]+arr[j]+arr[k])
 
