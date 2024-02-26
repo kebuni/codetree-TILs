3c19f96c0585b3cf18c6875081ce48a30786e5f8
kebuni <121942412+kebuni@users.noreply.github.com>
2024-02-27 03:01:06 +0900

    Title: 오목, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240227/오목/O-mok.py
+++ 240227/오목/O-mok.py
@@ -5,16 +5,18 @@ NOPE = 0
 BLACK = 1
 WHITE = 2
 
-grid = [list(map(int,input().split())) for i in range(N)]
+grid = [list(map(int, input().split())) for i in range(N)]
 
-combo = [0,0,0]
+combo = [0, 0, 0]
 last = NOPE
 
-def in_range(x,y):
-    return 0<=x<N and 0<=y<N
+
+def in_range(x, y):
+    return 0 <= x < N and 0 <= y < N
+
 
 for i in range(N):
-    combo = [0,0,0]
+    combo = [0, 0, 0]
     for j in range(N):
         cur = grid[i][j]
         if last == cur:
@@ -26,17 +28,15 @@ for i in range(N):
 
         if combo[BLACK] == 5:
             print(1)
-            print(i+1,j-2+1)
+            print(i + 1, j - 2 + 1)
             sys.exit(0)
         elif combo[WHITE] == 5:
             print(2)
-            print(i+1,j-2+1)
+            print(i + 1, j - 2 + 1)
             sys.exit(0)
-        
-        
 
 for j in range(N):
-    combo = [0,0,0]
+    combo = [0, 0, 0]
     for i in range(N):
         cur = grid[i][j]
         if last == cur:
@@ -45,21 +45,21 @@ for j in range(N):
             combo[cur] = 1
             combo[last] = 0
             last = cur
-        
+
         if combo[BLACK] == 5:
             print(1)
-            print(i-2+1,j+1)
+            print(i - 2 + 1, j + 1)
             sys.exit(0)
         elif combo[WHITE] == 5:
             print(2)
-            print(i-2+1,j+1)
+            print(i - 2 + 1, j + 1)
             sys.exit(0)
 
 for i in range(N):
     x = 0
     y = i
-    combo = [0,0,0]
-    while in_range(x,y):
+    combo = [0, 0, 0]
+    while in_range(x, y):
         cur = grid[x][y]
         if last == cur:
             combo[cur] += 1
@@ -70,20 +70,20 @@ for i in range(N):
 
         if combo[BLACK] == 5:
             print(1)
-            print(i-2+1,j+2+1)
+            print(x - 2 + 1, y + 2 + 1)
             sys.exit(0)
         elif combo[WHITE] == 5:
             print(2)
-            print(i-2+1,j+2+1)
+            print(x - 2 + 1, y + 2 + 1)
             sys.exit(0)
 
         x += 1
         y -= 1
-for i in range(1,N):
+for i in range(1, N):
     x = i
-    y = N-1
-    combo = [0,0,0]
-    while in_range(x,y):
+    y = N - 1
+    combo = [0, 0, 0]
+    while in_range(x, y):
         cur = grid[x][y]
         if last == cur:
             combo[cur] += 1
@@ -94,21 +94,21 @@ for i in range(1,N):
 
         if combo[BLACK] == 5:
             print(1)
-            print(i-2+1,j+2+1)
+            print(x - 2 + 1, y + 2 + 1)
             sys.exit(0)
         elif combo[WHITE] == 5:
             print(2)
-            print(i-2+1,j+2+1)
+            print(x - 2 + 1, y + 2 + 1)
             sys.exit(0)
 
         x += 1
         y -= 1
 
-for i in range(N-1,-1,-1):
+for i in range(N - 1, -1, -1):
     x = 0
     y = i
-    combo = [0,0,0]
-    while in_range(x,y):
+    combo = [0, 0, 0]
+    while in_range(x, y):
         cur = grid[x][y]
         if last == cur:
             combo[cur] += 1
@@ -119,20 +119,20 @@ for i in range(N-1,-1,-1):
 
         if combo[BLACK] == 5:
             print(1)
-            print(i-2+1,j-2+1)
+            print(x - 2 + 1, y - 2 + 1)
             sys.exit(0)
         elif combo[WHITE] == 5:
             print(2)
-            print(i-2+1,j-2+1)
+            print(x - 2 + 1, y - 2 + 1)
             sys.exit(0)
 
         x += 1
         y += 1
-for i in range(1,N):
+for i in range(1, N):
     x = i
     y = 0
-    combo = [0,0,0]
-    while in_range(x,y):
+    combo = [0, 0, 0]
+    while in_range(x, y):
         cur = grid[x][y]
         if last == cur:
             combo[cur] += 1
@@ -143,11 +143,11 @@ for i in range(1,N):
 
         if combo[BLACK] == 5:
             print(1)
-            print(i-2+1,j-2+1)
+            print(x - 2 + 1, y - 2 + 1)
             sys.exit(0)
         elif combo[WHITE] == 5:
             print(2)
-            print(i-2+1,j-2+1)
+            print(x - 2 + 1, y - 2 + 1)
             sys.exit(0)
 
         x += 1
