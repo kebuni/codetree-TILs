43b86fb4a9e07b5397da983836a9cadf435262e4
kebuni <121942412+kebuni@users.noreply.github.com>
2024-03-03 20:14:10 +0900

    Title: 바구니 안의 사탕 2, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240303/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240303/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -1,13 +1,13 @@
-line = [0 for i in range(101)]
+line = [0 for i in range(300)]
 
 N, K = map(int,input().split())
 ans = -1
 
 for i in range(N):
     a, b = map(int,input().split())
-    line[b] += a
+    line[b+100] += a
 
-for i in range(101-(2*K+1)+1):
+for i in range(300-(2*K+1)+1):
     sum = 0
     for j in range(2*K+1):
         sum += line[i+j]
