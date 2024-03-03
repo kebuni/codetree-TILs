6b46e7a1826d5fbd017cb4796e7366bff3eeba27
kebuni <121942412+kebuni@users.noreply.github.com>
2024-03-03 19:42:05 +0900

    Title: 아름다운 수열 2, Time: 249ms, Memory: 24MB, Status: Passed - CodeTree

--- 240303/아름다운 수열 2/beautiful-sequence-2.py
+++ 240303/아름다운 수열 2/beautiful-sequence-2.py
@@ -1,15 +1,25 @@
 N, M = map(int,input().split())
 
 A = list(map(int,input().split()))
-B = set(map(int,input().split()))
+B = list(map(int,input().split()))
+B_dic = {}
+
+for i in range(M):
+    if B[i] in B_dic:
+        B_dic[B[i]] += 1
+    else:
+        B_dic[B[i]] = 1
 
 ans = 0
 
 for i in range(N-M+1):
-    S = set()
+    A_dic = {}
     for k in range(M):
-        S.add(A[i+k])
-    if S == B:
+        if A[i+k] in A_dic:
+            A_dic[A[i+k]] += 1
+        else:
+            A_dic[A[i+k]] = 1
+    if A_dic == B_dic:
         ans += 1
 
 print(ans)
