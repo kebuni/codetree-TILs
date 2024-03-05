6796419916b72e45e6b0f4ce9340b19d873b14da
kebuni <121942412+kebuni@users.noreply.github.com>
2024-03-05 21:38:07 +0900

    Title: 기울어진 직사각형, Time: 218ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 240305/기울어진 직사각형/slanted-rectangle.py
@@ -0,0 +1,35 @@
+N = int(input())
+grid = [list(map(int,input().split())) for _ in range(N)]
+
+ans = 0
+
+def in_range(x,y):
+    return 0<=x<N and 0<=y<N
+
+def check_validity(x,y,r,l):
+    return in_range(x-r,y+r) and in_range(x-l,y-l) and in_range(x-r-l,y+r-l)
+
+def get_sum(x,y,r,l):
+
+    dxs = [-1,-1,1,1]
+    dys = [1,-1,-1,1]
+    dist = [r,l,r,l]
+
+    sum = 0
+    
+    for i in range(4):
+        for k in range(dist[i]):
+            x = x + dxs[i]
+            y = y + dys[i]
+            sum += grid[x][y]
+    
+    return sum
+
+for x in range(2,N):
+    for y in range(1,N-1):
+        for r in range(N):
+            for l in range(N):
+                if check_validity(x,y,r,l):
+                    ans = max(ans,get_sum(x,y,r,l))
+
+print(ans)
