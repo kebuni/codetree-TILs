803f6958378090860570a375aaa9efd350ccd52f
kebuni <121942412+kebuni@users.noreply.github.com>
2024-03-05 22:16:42 +0900

    Title: 양수 직사각형의 최대 크기, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240305/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
+++ 240305/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -1,33 +1,21 @@
-# 변수 선언 및 입력:
+N, M = map(int,input().split())
 
-n, m = tuple(map(int, input().split()))
-grid = [
-    list(map(int, input().split()))
-    for _ in range(n)
-]
+grid = [list(map(int,input().split())) for _ in range(N)]
 
+ans = 0
 
-# (x1, y1), (x2, y2)를 두 꼭지점으로 하는
-# 직사각형에 있는 값이 전부 양수인지 판단합니다.
-def positive_rect(x1, y1, x2, y2):
-    return all([
-        grid[i][j] > 0
-        for i in range(x1, x2 + 1)
-        for j in range(y1, y2 + 1)
-    ])
+def check(x1,y1,x2,y2):
+    for i in range(x1,x2+1):
+        for j in range(y1,y2+1):
+            if grid[i][j] <= 0:
+                return False
+    return True
 
+for x1 in range(N):
+    for y1 in range(M):
+        for x2 in range(x1,N):
+            for y2 in range(y1,M):
+                if check(x1,y1,x2,y2):
+                    ans = max(ans,(x2-x1+1)*(y2-y1+1))
 
-ans = -1
-
-# 직사각형의 양쪽 두 꼭지점 (i, j), (k, l)
-# 를 정하여
-# 해당 직사각형안에 있는 값이 전부 양수일 때만
-# 크기를 갱신합니다.
-for i in range(n):
-    for j in range(m):
-        for k in range(i, n):
-            for l in range(j, m):
-                if positive_rect(i, j, k, l):
-                    ans = max(ans, 
-                              (k - i + 1) * (l - j + 1))
 print(ans)
