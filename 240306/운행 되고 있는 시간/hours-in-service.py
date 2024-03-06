917888da5190f9fd2f482964eb90b54109b3f2a7
kebuni <121942412+kebuni@users.noreply.github.com>
2024-03-06 22:22:41 +0900

    Title: 운행 되고 있는 시간, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240306/운행 되고 있는 시간/hours-in-service.py
@@ -0,0 +1,15 @@
+N = int(input())
+employees = [tuple(map(int,input().split())) for _ in range(N)]
+ans = 0
+
+for i in range(N):
+    temp = [0 for _ in range(101)]
+    for j in range(N):
+        if j == i:
+            continue
+        s, e = employees[j]
+        for t in range(s,e):
+            temp[t] = 1
+    ans = max(ans,sum(temp))
+
+print(ans)
