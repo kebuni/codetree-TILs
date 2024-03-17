8c573e4f6433c54b7ca38559e53c6449d18f694b
kebuni <121942412+kebuni@users.noreply.github.com>
2024-03-18 00:47:04 +0900

    Title: 청소는 즐거워, Time: 5095ms, Memory: 27MB, Status: Time Limit Exceed - CodeTree

--- 240318/청소는 즐거워/cleaning-is-joyful.py
+++ 240318/청소는 즐거워/cleaning-is-joyful.py
@@ -1,17 +1,28 @@
-# 변수 선언 및 입력
-n = int(input())
-grid = [
-    list(map(int, input().split()))
-    for _ in range(n)
-]
+import math
 
-# 시작 위치와 방향, 
-# 해당 방향으로 이동할 횟수를 설정합니다.
-curr_x, curr_y = n // 2, n // 2
-move_dir, move_num = 0, 1
+N = int(input())
+grid = [list(map(int,input().split())) for i in range(N)]
+delta = [[0 for i in range(N)] for j in range(N)]
 
-ans = 0
+dist_list = []
+for i in range(1,N):
+    dist_list.append(i)
+    dist_list.append(i)
+dist_list.append(N-1)
 
+dxs = [0,1,0,-1]
+dys = [-1,0,1,0]
+out_of_grid_dust = 0
+# sweep_info = [(0,-2,0.05),
+#               (-1,-1,0.1),
+#               (1,-1,0.1),
+#               (-2,0,0.02),
+#               (-1,0,0.07),
+#               (1,0,0.07),
+#               (2,0,0.02),
+#               (-1,1,0.01),
+#               (1,1,0.01)]
+sweep_infos = []
 dust_ratio = [
     [
         [0,  0, 2, 0, 0],
@@ -43,68 +54,71 @@ dust_ratio = [
     ]
 ]
 
+##########################################
 
-def in_range(x, y):
-    return 0 <= x and x < n and 0 <= y and y < n
+def in_range(x,y):
+    return 0<=x<N and 0<=y<N
 
+def sweep(x,y,d):
+    global out_of_grid_dust
+    clear_delta()
 
-# (x, y) 위치에 dust 만큼의 먼지를 추가합니다.
-def add_dust(x, y, dust):
-    global ans
-    
-    # 격자 범위를 벗어난다면 답에 더해줍니다.
-    if not in_range(x, y):
-        ans += dust
-    # 격자 범위 안이라면, 해당 칸에 더해줍니다.
+    #sweep_grid = get_sweep_grid(d)
+
+    cur_dust = grid[x][y]
+    moved_dust = 0
+    for dx in range(-2,3):
+        for dy in range(-2,3):
+            nx,ny = x + dx, y+dy
+            if in_range(nx,ny):
+                delta[nx][ny] = cur_dust * dust_ratio[d][dx+2][dy+2] // 100
+                moved_dust += cur_dust * dust_ratio[d][dx+2][dy+2] // 100
+            else:
+                out_of_grid_dust += cur_dust * dust_ratio[d][dx+2][dy+2] // 100
+                moved_dust += cur_dust * dust_ratio[d][dx + 2][dy + 2] // 100
+
+    nx = x + dxs[d]
+    ny = y + dys[d]
+    if in_range(nx,ny):
+        delta[nx][ny] = cur_dust - moved_dust
     else:
-        grid[x][y] += dust
+        out_of_grid_dust += cur_dust - moved_dust
+    delta[x][y] = -cur_dust
+
+    sum_delta()
+
+    return
 
 
-# 한 칸 움직이며 청소를 진행합니다.
-def move():
-    global curr_x, curr_y
-    
-    # 문제에서 원하는 진행 순서대로 
-    # 왼쪽 아래 오른쪽 위 방향이 되도록 정의합니다.
-    dxs, dys = [0, 1, 0, -1], [-1, 0, 1, 0]
-    
-    # curr 위치를 계산합니다.
-    curr_x, curr_y = curr_x + dxs[move_dir], curr_y + dys[move_dir]
-    
-    # 현재 위치를 기준으로 각 위치에 먼지를 더해줍니다.
-    added_dust = 0
-    for i in range(5):
-        for j in range(5):
-            dust = grid[curr_x][curr_y] * dust_ratio[move_dir][i][j] // 100
-            add_dust(curr_x + i - 2, curr_y + j - 2, dust)
-            
-            added_dust += dust
-    
-    # a% 자리에 먼지를 추가합니다.
-    add_dust(curr_x + dxs[move_dir], curr_y + dys[move_dir], 
-             grid[curr_x][curr_y] - added_dust)
 
+def clear_delta():
+    for x in range(N):
+        for y in range(N):
+            delta[x][y] = 0
 
-def end():
-    return not curr_x and not curr_y
+def sum_delta():
+    for x in range(N):
+        for y in range(N):
+            grid[x][y] += delta[x][y]
 
+def print_grid():
+    for x in range(N):
+        for y in range(N):
+            print(grid[x][y],end=' ')
+        print()
 
-while not end():
-    # move_num 만큼 이동합니다.
-    for _ in range(move_num):
-        move()
-        
-        # 이동하는 도중 (0, 0)으로 오게 되면,
-        # 움직이는 것을 종료합니다.
-        if end():
-            break
-    
-    # 방향을 바꿉니다.
-    move_dir = (move_dir + 1) % 4
-    # 만약 현재 방향이 왼쪽 혹은 오른쪽이 된 경우에는
-    # 특정 방향으로 움직여야 할 횟수를 1 증가시킵니다.
-    if move_dir == 0 or move_dir == 2:
-        move_num += 1
+##########################################
 
-# 출력:
-print(ans)
+cur_x, cur_y = N//2, N//2
+cur_d = 0
+
+for dist in dist_list:
+    for i in range(dist):
+        cur_x = cur_x + dxs[cur_d]
+        cur_y = cur_y + dys[cur_d]
+        sweep(cur_x,cur_y,cur_d)
+        #print_grid()
+        #print(out_of_grid_dust)
+    cur_d = (cur_d + 1) % 4
+
+print(out_of_grid_dust)
