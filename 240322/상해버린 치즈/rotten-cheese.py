79c1532d0975453343f5fd474278d4e5346e9d50
kebuni <121942412+kebuni@users.noreply.github.com>
2024-03-22 20:17:34 +0900

    Title: 상해버린 치즈, Time: 181ms, Memory: 25MB, Status: Passed - CodeTree

--- 240322/상해버린 치즈/rotten-cheese.py
+++ 240322/상해버린 치즈/rotten-cheese.py
@@ -23,14 +23,20 @@ for p, t in sick_list:
                 new_cadidate.add(c)
     cadidate = new_cadidate
 
+#print("final candidate : ",cadidate)
+
 # 상할 수 있는 치즈 후보를 추렸습니다 이제 먹은 사람에게 약을 줍니다
 ans = 0
 for c in cadidate:
     temp = 0
+    need_medicine = [False for i in range(N+1)]
     for p in range(1,N+1):
         for t in range(1,T+1):
             if c in table[p][t]:
-                temp += 1
+                need_medicine[p] = True
+
+    temp = sum(list(map(lambda x:1 if x else 0,need_medicine)))
+    #print("for",c,"medicine",temp)
     ans = max( ans, temp)
 
 print(ans)
