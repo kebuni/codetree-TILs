a4d5977e7188e40bbd053d5d4eaf2f01312664d9
kebuni <121942412+kebuni@users.noreply.github.com>
2024-03-24 01:00:52 +0900

    Title: 트리 최적의 노드, Time: 1630ms, Memory: 221MB, Status: Passed - CodeTree

---
+++ 240324/트리 최적의 노드/tree-optimal-node.py
@@ -0,0 +1,53 @@
+import sys
+sys.setrecursionlimit(100000)
+
+N = int(input())
+
+edge = [[] for i in range(N)]
+dist_list = [0 for i in range(N)]
+visited = [False for i in range(N)]
+
+for i in range(N-1):
+    a, b = map(int,input().split())
+    edge[a-1].append((b-1,1))
+    edge[b-1].append((a-1,1))
+
+############################
+
+def dfs(cur):
+    for next, dist in edge[cur]:
+        if not visited[next]:
+            dist_list[next] = dist_list[cur] + dist
+            push(next)
+            dfs(next)
+    return
+
+def push(cur):
+    visited[cur] = True
+    return
+
+############################
+
+push(0)
+dfs(0)
+
+max_idx = 0
+max_dist = 0
+for i in range(N):
+    if dist_list[i] > max_dist:
+        max_idx = i
+        max_dist = dist_list[i]
+
+visited = [False for i in range(N)]
+dist_list = [0 for i in range(N)]
+push(max_idx)
+dfs(max_idx)
+
+max_idx = 0
+max_dist = 0
+for i in range(N):
+    if dist_list[i] > max_dist:
+        max_idx = i
+        max_dist = dist_list[i]
+
+print((max_dist+1)//2)
