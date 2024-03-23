61ca2ef6e9335ca23c6d30cd5b34e2e043b76edd
kebuni <121942412+kebuni@users.noreply.github.com>
2024-03-23 23:53:41 +0900

    Title: 트리 파악, Time: 1022ms, Memory: 153MB, Status: Passed - CodeTree

--- 240323/트리 파악/identifying-the-tree.py
+++ 240323/트리 파악/identifying-the-tree.py
@@ -1,13 +1,23 @@
 import sys
 sys.setrecursionlimit(100000)
 N = int(input())
-
+#print(N)
 edge = [[] for i in range(N+1)]
 visited = [False for i in range(N+1)]
-depth_list = [-1 for i in range(N+1)]
-is_leaf = [False for i in range(N+1)]
+# depth_list = [-1 for i in range(N+1)]
+# is_leaf = [False for i in range(N+1)]
+result = 0
+#print(N)
+for i in range(N-1):
+    a, b = map(int,input().split())
+    #print(a,b)
+    edge[a].append(b)
+    edge[b].append(a)
+
+###################################3
 
 def dfs(cur,depth):
+    global result
     children_num = 0
     for next in edge[cur]:
         if not visited[next]:
@@ -15,26 +25,18 @@ def dfs(cur,depth):
             push(next,depth+1)
             dfs(next,depth+1)
     if not children_num:
-        is_leaf[cur] = True
-
+        result += depth
     return
 
 def push(cur,depth):
-    depth_list[cur] = depth
+    #depth_list[cur] = depth
     visited[cur] = True
     return
 
-for i in range(N-1):
-    a,b  = map(int,input().split())
-    edge[a].append(b)
-    edge[b].append(a)
+##############################3
 
 push(1,0)
 dfs(1,0)
 # print(depth_list)
 # print(is_leaf)
-sum = 0
-for i in range(1,N+1):
-    if is_leaf[i]:
-        sum += depth_list[i]
-print((sum) % 2)
+print((result) % 2)
