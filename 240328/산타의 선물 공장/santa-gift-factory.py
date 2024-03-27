db45d63c6e71b96876a018f62b09f2f37fb4670e
kebuni <121942412+kebuni@users.noreply.github.com>
2024-03-28 01:52:21 +0900

    Title: 산타의 선물 공장, Time: 1149ms, Memory: 67MB, Status: Passed - CodeTree

--- 240328/산타의 선물 공장/santa-gift-factory.py
+++ 240328/산타의 선물 공장/santa-gift-factory.py
@@ -1,143 +1,257 @@
-import sys
+from collections import defaultdict
 
-WEIGHT = 0
-NEXT = 1
-END = -2
+MAX_M = 10
 
-Q = int(input())
-first_q = list(map(int,sys.stdin.readline().split()))
-N = first_q[1]
-M = first_q[2]
-belt = [{-1:[-1,-2],-2:[-2,-3]} for i in range(M)]
-tail = [-1 for i in range(M)]
-malfunction = [False for i in range(M)]
+# 변수 선언
+n, m, q = -1, -1, -1
 
-for belt_idx in range(M):
-    offset1 = belt_idx*(N//M)+3
-    offset2 = belt_idx*(N//M)+N+3
-    for i in range(N//M):
-        id = first_q[offset1+i]
-        w = first_q[offset2+i]
-        if i == 0:
-            belt[belt_idx][-1][NEXT] = id
-            belt[belt_idx][id] = [w,first_q[offset1+i+1]]
-        elif i == N//M-1:
-            tail[belt_idx] = id
-            belt[belt_idx][id] = [w,-2]
-        else:
-            belt[belt_idx][id] = [w,first_q[offset1+i+1]]
+# 각 id별로 상자 무게를 저장합니다.
+weight = {}
 
-query = [list(map(int,input().split())) for i in range(Q-1)]
+# id에 해당하는 상자의 nxt값과 prv값을 관리합니다.
+# 0이면 없다는 뜻입니다.
+prv, nxt = defaultdict(lambda: 0), defaultdict(lambda: 0)
 
-#################
+# 각 벨트별로 head, tail id를 관리합니다.
+# 0이면 없다는 뜻입니다.
+head = [0] * MAX_M
+tail = [0] * MAX_M
 
-def process200(wmax):
-    weight_sum = 0
-    for bi in range(M):
-        if malfunction[bi]:
-            continue
-        cur_head = belt[bi][-1][NEXT]
-        if cur_head == -2:
-            continue
-        cur_weight = belt[bi][cur_head][WEIGHT]
-        belt[bi][-1][NEXT] = belt[bi][cur_head][NEXT]
-        if cur_weight <= wmax: #하차하기
-            weight_sum += cur_weight
-            belt[bi].pop(cur_head)
-        else: #맨뒤로 옮기기
-            cur_tail = tail[bi]
-            belt[bi][cur_tail][NEXT] = cur_head
-            belt[bi][cur_head][NEXT] = END
-            tail[bi] = cur_head
-    print(weight_sum)
-    return
+# 벨트가 망가졌는지를 표시합니다.
+broken = [False] * MAX_M
 
-def process300(rid):
-    result = False
+# 물건 별로 벨트 번호를 기입합니다.
+# 벨트 번호가 -1이면 사라진 것입니다.
+belt_num = defaultdict(lambda: -1)
 
-    for bi in range(M):
-        if malfunction[bi]:
-            continue
-        if rid in belt[bi]:
-            result = True
-            prev = belt[bi][-1][NEXT]
-            rid_next = belt[bi][rid][NEXT]
-            while belt[bi][prev][NEXT] != rid:
-                prev = belt[bi][prev][NEXT]
-            belt[bi][prev][NEXT] = rid_next
-            if tail[bi] == rid:
-                tail[bi] = prev
-            belt[bi].pop(rid)
 
-    if result:
-        print(rid)
+# 공장 설립
+def build_factory(elems):
+    global n, m
+    
+    # 공장 정보를 입력받습니다.
+    n, m = elems[1], elems[2]
+    ids, ws = elems[3:3+n], elems[3+n:3+n+n]
+    
+    # id마다 무게를 관리합니다.
+    for i in range(n):
+        weight[ids[i]] = ws[i]
+    
+    # 벨트 별로 상자 목록을 넣어줍니다.
+    size = n // m
+    for i in range(m):
+        # head, tail을 설정해줍니다.
+        head[i] = ids[i * size]
+        tail[i] = ids[(i + 1) * size - 1]
+        for j in range(i * size, (i + 1) * size):
+            # 상자 ID마다 벨트 번호를 기입합니다.
+            belt_num[ids[j]] = i
+
+            # nxt, prv를 설정해줍니다.
+            if j < (i + 1) * size - 1:
+                nxt[ids[j]] = ids[j + 1]
+                prv[ids[j + 1]] = ids[j]
+
+
+# Id에 해당하는 상자를 삭제합니다
+def remove_id(_id, remove_belt):
+    b_num = belt_num[_id]
+    # 벨트 번호를 제거해줍니다.
+    if remove_belt:
+        belt_num[_id] = -1
+
+    # 하나 남은 원소라면
+    # head, tail이 사라지고 끝납니다.
+    if head[b_num] == tail[b_num]:
+        head[b_num] = tail[b_num] = 0
+
+    # 삭제 되는게 head라면
+    # head만 변경되고 끝납니다.
+    elif _id == head[b_num]:
+        nid = nxt[_id]
+        head[b_num] = nid
+        prv[nid] = 0
+    # 삭제 되는게 tail이라면
+    # tail만 변경되고 끝납니다.
+    elif _id == tail[b_num]:
+        pid = prv[_id]
+        tail[b_num] = pid
+        nxt[pid] = 0
+    # 중간에 있는 id가 삭제되는 것이라면
+    # nxt, prv만 수선해줍니다.
     else:
-        print(-1)
-    return
+        pid, nid = prv[_id], nxt[_id]
+        nxt[pid] = nid
+        prv[nid] = pid
 
-def process400(fid):
-    result_bi = -1
-    for bi in range(M):
-        if malfunction[bi]:
+    # nxt, prv값을 지워줍니다.
+    nxt[_id] = prv[_id] = 0
+
+
+# target_id 바로 뒤에
+# id를 추가합니다.
+def push_id(target_id, _id):
+    nxt[target_id] = _id
+    prv[_id] = target_id
+
+    # 만약 target_id가 tail이었다면
+    # tail을 변경해줍니다.
+    b_num = belt_num[target_id]
+    if tail[b_num] == target_id:
+        tail[b_num] = _id
+
+
+# 물건 하차
+def drop_off(elems):
+    w_max = elems[1]
+    
+    # 각 벨트마다 보며
+    # 첫 번째 상자를 열어봅니다.
+    w_sum = 0
+    for i in range(m):
+        # 망가진 벨트라면 넘어갑니다.
+        if broken[i]:
             continue
-        if fid in belt[bi]:
-            result_bi = bi + 1
-            cur_head = belt[bi][-1][NEXT]
-            cur_tail = tail[bi]
-            prev = cur_head
-            while belt[bi][prev][NEXT] != fid:
-                prev = belt[bi][prev][NEXT]
-            belt[bi][-1][NEXT] = fid
-            belt[bi][cur_tail][NEXT] = cur_head
-            tail[bi] = prev
-            belt[bi][prev][NEXT] = END
 
-    print(result_bi)
-    return
+        # 벨트의 head를 확인합니다.
+        if head[i] != 0:
+            _id = head[i]
+            w = weight[_id]
 
-def process500(bnum):
-    if malfunction[bnum]:
+            # 가장 앞에 있는 상자의 무게가 w_max 이하라면
+            # 하차시키고 답에 더해줍니다.
+            if w <= w_max:
+                w_sum += w
+
+                # 하차를 진행합니다.
+                remove_id(_id, True)
+            # 그렇지 않다면
+            # 상자를 맨 뒤로 올려줍니다.
+            elif nxt[_id] != 0:
+                # 제거해준 뒤
+                remove_id(_id, False)
+                
+                # 맨 뒤에 push해줍니다.
+                push_id(tail[i], _id)
+
+    # 하차한 상자의 무게 합을 출력합니다.
+    print(w_sum)
+
+
+# 물건 제거
+def remove(elems):
+    r_id = elems[1]
+
+    # 이미 삭제된 상자라면
+    # -1을 출력하고 패스합니다.
+    if belt_num[r_id] == -1:
+        print(-1)
+        return
+        
+    # 해당 상자를 제거합니다.
+
+    remove_id(r_id, True)
+    print(r_id)
+    
+
+# 물건 확인
+def find(elems):
+    f_id = elems[1]
+
+    # 이미 삭제된 상자라면
+    # -1을 출력하고 패스합니다.
+    if belt_num[f_id] == -1:
         print(-1)
         return
 
-    malfunction[bnum] = True
-    bi = bnum
-    for i in range(1,M):
-        bi = (bnum + i) % M
-        if not malfunction[bi]:
+    # 해당 상자를 찾아
+    # 이를 맨 앞으로 당겨줍니다.
+    # head가 아닌 경우에만 유효합니다.
+    b_num = belt_num[f_id]
+    if head[b_num] != f_id:
+        orig_tail = tail[b_num]
+        orig_head = head[b_num]
+
+        # 새로 tail을 갱신해줍니다.
+        now_tail = prv[f_id]
+        tail[b_num] = now_tail
+        nxt[now_tail] = 0
+
+        # 기존 tail의 nxt를 head로,
+        # heda의 prv를 기존 tail로 만들어줍니다.
+        nxt[orig_tail] = orig_head
+        prv[orig_head] = orig_tail
+
+        # 새로 head를 지정합니다.
+        head[b_num] = f_id
+
+    # 해당 ID의 belt 번호를 출력합니다.
+    print(b_num + 1)
+
+
+# 벨트 고장
+def broken_belt(elems):
+    b_num = elems[1]
+    b_num -= 1
+
+    # 이미 망가져 있다면
+    # -1을 출력하고 패스합니다.
+    if broken[b_num]:
+        print(-1)
+        return
+
+    broken[b_num] = 1
+
+    # 만약 빈 벨트라면 패스합니다.
+    if head[b_num] == 0:
+        print(b_num + 1)
+        return
+
+    # 오른쪽으로 돌면서
+    # 아직 망가지지 않은 벨트 위로 상자를 전부 옮겨줍니다.
+    nxt_num = b_num
+    while True:
+        nxt_num = (nxt_num + 1) % m
+        # 최초로 망가지지 않은 곳이 보이면
+        if not broken[nxt_num]:
+            # 만약 해당 벨트가 비어있다면
+            # 그대로 옮겨줍니다.
+            if tail[nxt_num] == 0:
+                head[nxt_num] = head[b_num]
+                tail[nxt_num] = tail[b_num]
+            else:
+                # 해당 위치로 상자를 전부 옮겨줍니다.
+                # head만 tail뒤에 붙여준 뒤
+                push_id(tail[nxt_num], head[b_num])
+                # tail만 갈아껴주면 됩니다.
+                tail[nxt_num] = tail[b_num]
+
+            # head부터 tail까지 보며
+            # belt_num을 갱신해줍니다.
+            _id = head[b_num]
+            while _id != 0:
+                belt_num[_id] = nxt_num
+                _id = nxt[_id]
+
+            head[b_num] = tail[b_num] = 0
             break
 
-    cur_tail = tail[bi]
-    cur_head = belt[bnum][-1][NEXT]
-    while belt[bnum][cur_head][NEXT] != -3:
-        w = belt[bnum][cur_head][WEIGHT]
-        next = belt[bnum][cur_head][NEXT]
-        belt[bi][cur_tail][NEXT] = cur_head
-        belt[bi][cur_head] = [w,next]
-        belt[bnum].pop(cur_head)
-        cur_tail = cur_head
-        cur_head = next
-    tail[bi] = cur_tail
-    tail[bnum] = END
-    belt[bi][cur_tail][NEXT] = END
-    belt[bnum][-1][NEXT] = END
+    print(b_num + 1)
 
-    print(bnum+1)
 
-    return
+# 입력:
+q = int(input())
+for _ in range(q):
+    elems = list(map(int, input().split()))
+    q_type = elems[0]
 
-#################
-
-for q, num in query:
-    if q == 200:
-        process200(num)
-    elif q == 300:
-        process300(num)
-    elif q == 400:
-        process400(num)
+    if q_type == 100:
+        build_factory(elems)
+    elif q_type == 200:
+        drop_off(elems)
+    elif q_type == 300:
+        remove(elems)
+    elif q_type == 400:
+        find(elems)
     else:
-        process500(num-1)
-
-    # for i in range(M):
-    #     print(belt[i])
-    # print(tail)
+        broken_belt(elems)
