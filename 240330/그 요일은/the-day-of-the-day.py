a2cab3bba896964ca1818a5cd1ed712fd8ccaf41
kebuni <121942412+kebuni@users.noreply.github.com>
2024-03-30 20:52:19 +0900

    Title: 그 요일은, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240330/그 요일은/the-day-of-the-day.py
+++ 240330/그 요일은/the-day-of-the-day.py
@@ -2,7 +2,7 @@ m1, d1, m2, d2 = map(int,input().split())
 target_date = input()
 
 num_of_days = [0,31,29,31,30,31,30,31,31,30,31,30,31]
-date_to_num = {'Mon':0, 'Two':1, 'Wed':2, 'Thu':3, 'Fri':4, 'Sat':5, 'Sun':6}
+date_to_num = {'Mon':0, 'Tue':1, 'Wed':2, 'Thu':3, 'Fri':4, 'Sat':5, 'Sun':6}
 
 sum1 = d1
 for i in range(m1):
