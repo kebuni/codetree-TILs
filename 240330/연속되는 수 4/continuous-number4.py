be59af08094cd4940425d1d9ce1400c3ce91f978
kebuni <121942412+kebuni@users.noreply.github.com>
2024-03-30 21:22:39 +0900

    Title: 연속되는 수 4, Time: 138ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240330/연속되는 수 4/continuous-number4.py
@@ -0,0 +1,11 @@
+N = int(input())
+arr = [int(input()) for i in range(N)]
+
+ans, cnt = 0, 0
+for i in range(N):
+    if i > 0 and arr[i]>arr[i-1]:
+        cnt += 1
+    else:
+        cnt = 1
+    ans = max(ans,cnt)
+print(ans)
