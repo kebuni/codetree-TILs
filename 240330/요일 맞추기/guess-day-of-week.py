048b919ed32e7b8cde2f8ff7d758d9eed30bdcc3
kebuni <121942412+kebuni@users.noreply.github.com>
2024-03-30 20:43:15 +0900

    Title: 요일 맞추기, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240330/요일 맞추기/guess-day-of-week.py
@@ -0,0 +1,14 @@
+m1, d1, m2, d2 = map(int,input().split())
+
+num_of_days = [0,31,28,31,30,31,30,31,31,30,31,30,31]
+
+sum1 = d1
+for i in range(m1):
+    sum1 += num_of_days[i]
+
+sum2 = d2
+for i in range(m2):
+    sum2 += num_of_days[i]
+
+date = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun']
+print(date[(sum2-sum1)%7])
