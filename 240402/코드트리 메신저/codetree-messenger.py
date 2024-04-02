9cb8f349737e3ea8c613cf9372da0b3c83c74ee3
kebuni <121942412+kebuni@users.noreply.github.com>
2024-04-02 22:03:19 +0900

    Title: 코드트리 메신저, Time: 2349ms, Memory: 80MB, Status: Passed - CodeTree

--- 240402/코드트리 메신저/codetree-messenger.py
+++ 240402/코드트리 메신저/codetree-messenger.py
@@ -1,59 +1,104 @@
 from collections import deque
 
-N, Q = map(int,input().split())
-children = [{} for i in range(N+1)]
+MAX_N = 100001
+MAX_D = 22
+authority, parent, val = [0] * MAX_N, [0] * MAX_N, [0] * MAX_N
+alarm = [False for i in range(MAX_N)]
+nx = [[0 for i in range(MAX_D)] for j in range(MAX_N)]
+# nx[n][d] = n번째 채팅방이 d depth에 알림을 줄 수 있음
 
+N, Q = map(int,input().split())
 command = list(map(int,input().split()))
-parent = [-1] + command[1:N+1]
-authority = [-1] + command[N+1:]
+for i in range(1,N+1):
+    parent[i] = command[i]
+for i in range(1,N+1):
+    authority[i] = command[i + N]
+    if authority[i] > 20:
+        authority[i] = 20
 
 for i in range(1,N+1):
-    p = parent[i]
-    children[p][i] = True
+    cur = i
+    x = authority[i]
+    nx[cur][x] += 1
+    while parent[cur] and x:
+        cur = parent[cur]
+        x -= 1
+        if x:
+            nx[cur][x] += 1
+        val[cur] += 1
 
 #######################################
 
 def switch_alarm(c):
-    p = parent[c]
-    if children[p][c] == True:
-        children[p][c] = False
-    else:
-        children[p][c] = True
+    cur = parent[c]
+    num = 1
+    while cur:
+        for i in range(num,22):
+            val[cur] += nx[c][i] if alarm[c] else -nx[c][i]
+            if i > num:
+                nx[cur][i-num] += nx[c][i] if alarm[c] else -nx[c][i]
+        if alarm[cur]:
+            break
+        cur = parent[cur]
+        num += 1
+    alarm[c] = not alarm[c]
     return
 
 def change_authority(c,power):
+    bef_power = authority[c]
+    power = min(power, 20)  # 권한의 크기를 20으로 제한합니다.
     authority[c] = power
+
+    nx[c][bef_power] -= 1
+    if not alarm[c]:
+        cur = parent[c]
+        num = 1
+        # 상위 채팅으로 이동하며 nx와 val 값을 갱신합니다.
+        while cur:
+            if bef_power >= num:
+                val[cur] -= 1
+            if bef_power > num:
+                nx[cur][bef_power - num] -= 1
+            if alarm[cur]:
+                break
+            cur = parent[cur]
+            num += 1
+
+    nx[c][power] += 1
+    if not alarm[c]:
+        cur = parent[c]
+        num = 1
+        # 상위 채팅으로 이동하며 nx와 val 값을 갱신합니다.
+        while cur:
+            if power >= num:
+                val[cur] += 1
+            if power > num:
+                nx[cur][power - num] += 1
+            if alarm[cur]:
+                break
+            cur = parent[cur]
+            num += 1
     return
 
 def switch_parent(c1,c2):
-    p1 = parent[c1]
-    p2 = parent[c2]
+    original_alarm1 = alarm[c1]
+    original_alarm2 = alarm[c2]
 
-    c1_alarm = children[p1][c1]
-    c2_alarm = children[p2][c2]
+    if not alarm[c1]:
+        switch_alarm(c1)
+    if not alarm[c2]:
+        switch_alarm(c2)
 
-    children[p1].pop(c1)
-    children[p2].pop(c2)
+    parent[c1], parent[c2] = parent[c2], parent[c1]
 
-    children[p1][c2] = c2_alarm
-    children[p2][c1] = c1_alarm
-
-    parent[c1] = p2
-    parent[c2] = p1
+    if not original_alarm1:
+        switch_alarm(c1)
+    if not original_alarm2:
+        switch_alarm(c2)
     return
 
 def find_alarm(c):
-    ans = 0
-    stack = deque()
-    stack.append((c,0))
-    while stack:
-        x, dist = stack.pop()
-        if x != c and dist <= authority[x]:
-            ans += 1
-        for next, alarm in children[x].items():
-            if alarm:
-                stack.append((next, dist+1))
-    print(ans)
+    print(val[c])
     return
 
 #######################################
