4111e1092957eb50e82424c4cdfd4299b5f90b2e
kebuni <121942412+kebuni@users.noreply.github.com>
2024-12-06 21:23:45 +0900

    Title: 낮은 지점들, Time: 62ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 241206/낮은 지점들/lowest-points.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+unordered_map<int,int> m;
+
+int main() {
+    
+    int N;
+    cin >> N;
+
+    for (int i=0; i<N;i++){
+        int x, y;
+        cin >> x >> y;
+        if(m.find(x)==m.end())
+            m[x] = y;
+        else
+            m[x] = m[x] > y ? y : m[x];
+        //cout << m[x] << endl;
+    }
+
+    int ans=0;
+    for (auto it:m)
+        ans += it.second;
+    cout << ans;
+
+    return 0;
+}
